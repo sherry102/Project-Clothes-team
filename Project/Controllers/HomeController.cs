ca7adfdffa3b7f68084ad66673cc6643b900518b
joey <joey4755@gmail.com>
2024-12-23 23:28:17 +0800

    Home-Index 修正

--- Project/Controllers/HomeController.cs
+++ Project/Controllers/HomeController.cs
@@ -17,8 +17,36 @@ namespace Project.Controllers
 
         public IActionResult Index()
         {
-            return View();
-            // ด๚ธี
+            DbuniPayContext db = new DbuniPayContext();
+
+            var today = DateTime.Today;
+            var currentMonth = DateTime.Today.Month;
+            var currentYear = DateTime.Today.Year;
+
+            List<Torder> order = db.Torders.ToList();
+            List<TorderDetail> orderDetail = db.TorderDetails.ToList();
+			List<TorderDetail> SalesCount_TOP5 = db.TorderDetails.OrderByDescending(t => t.Odcounts).Take(5).ToList();
+
+			int totalprice_today = db.Torders.Where(t=>t.Odate.Date==today).Sum(t => t.OtotalPrice);
+            int totalprice_month = db.Torders.Where(t => t.Odate.Month==currentMonth && t.Odate.Year==currentYear).Sum(t => t.OtotalPrice);
+            int totalprice_year = db.Torders.Where(t => t.Odate.Year == currentYear).Sum(t => t.OtotalPrice);
+
+            int totalsales_today = db.Torders.Where(t => t.Odate.Date == today).Count();
+            int totalsales_month = db.Torders.Where(t => t.Odate.Month == currentMonth && t.Odate.Year == currentYear).Count();
+            int totalsales_year = db.Torders.Where(t => t.Odate.Year == currentYear).Count();
+
+            var viewModel = new IndexViewModel {
+            Torder=order,
+            TorderDetail=orderDetail,
+            TotalPrice_Today= totalprice_today,
+            TotalPrice_Month=totalprice_month,
+            TotalPrice_Year=totalprice_year,
+            TotalSales_Today=totalsales_today,
+            TotalSales_Month=totalsales_month,
+            TotalSales_Year=totalsales_year,
+            SalesCount_TOP5= SalesCount_TOP5
+			};
+            return View(viewModel);
         }
 
         public IActionResult Login()
