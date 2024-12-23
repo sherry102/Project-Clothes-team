decff9e4bda8ce3daeef4f7f8d33fc6356dc0a28
joey <joey4755@gmail.com>
2024-12-23 23:29:09 +0800

    新增IndexViewModel

---
+++ Project/ViewModel/IndexViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Project.Models;
+
+namespace Project.ViewModel
+{
+    public class IndexViewModel
+    {
+        public List<Torder> Torder { get; set; }
+
+        public List<TorderDetail> TorderDetail { get; set; }
+
+        public int TotalPrice_Today { get; set; }
+
+        public int TotalPrice_Month { get; set; }
+
+        public int TotalPrice_Year { get; set; }
+
+        public int TotalSales_Today { get; set; }
+
+        public int TotalSales_Month { get; set; }
+
+        public int TotalSales_Year { get; set; }
+
+        public List<TorderDetail> SalesCount_TOP5 { get; set; }
+
+	}
+}
