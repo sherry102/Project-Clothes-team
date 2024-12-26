3a149205b4e59205f617d1dc26c8d90495d4d7e7
ASUS <ASUS@LAPTOP-BENV5S2Q>
2024-12-26 10:55:12 +0800

    123

---
+++ Project/Controllers/OrderController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project.Models;
+
+namespace Project.Controllers
+{
+    public class OrderController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult List()
+        { 
+            var db = new DbuniPayContext(); 
+            var orders = db.Torders.ToList(); 
+             
+            if (orders == null || !orders.Any())
+            { 
+                return View(new List<OrderWrap>()); //沒有資料則返回錯誤頁面或空頁面
+            }  
+
+            List<OrderWrap> orderWrapList = new List<OrderWrap>(); 
+            foreach (var order in orders)
+            { 
+                if (order == null) continue; 
+                OrderWrap wrap = new OrderWrap()
+                {
+                    Oid = order.Oid,
+                    Oname = order.Oname,
+                    Odiscountedprice = order.Odiscountedprice,
+                    OtotalPrice = order.OtotalPrice,
+                    Odate = order.Odate,
+                    Mid = order.Mid,
+                    Oaddress = order.Oaddress,
+                    Ophone = order.Ophone,
+                    Ostatus = order.Ostatus,
+                    Opayment = order.Opayment
+                }; 
+                orderWrapList.Add(wrap);
+            } 
+            return View(orderWrapList);
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+                return RedirectToAction("List");
+
+            DbuniPayContext db = new DbuniPayContext();
+            Torder x = db.Torders.FirstOrDefault(c => c.Oid == id);
+            if (x == null)
+                return RedirectToAction("List");
+
+            return View(new OrderWrap() { order = x });
+        }
+        [HttpPost]
+        public IActionResult Edit(OrderWrap p)
+        {
+            DbuniPayContext db = new DbuniPayContext();
+            Torder x = db.Torders.FirstOrDefault(c => c.Oid == p.Oid);
+            if (x != null)
+            { 
+                x.Oname = p.Oname;
+                x.Odiscountedprice = p.Odiscountedprice;
+                x.OtotalPrice = p.OtotalPrice;
+                x.Odate = p.Odate;
+                x.Mid = p.Mid;
+                x.Oaddress = p.Oaddress;
+                x.Ophone = p.Ophone;
+                x.Ostatus = p.Ostatus;
+                x.Opayment = p.Opayment;
+                db.SaveChanges();
+            }
+            return RedirectToAction("List");
+        }
+    }
+}
---
+++ Project/Models/OrderWrap.cs
@@ -0,0 +1,80 @@
+﻿using System.ComponentModel;
+
+namespace Project.Models
+{
+    public class OrderWrap
+    {
+        private Torder _order ;
+
+        public Torder order
+        {
+            get { return _order; }
+            set { _order = value; }
+        }
+
+        public OrderWrap()
+        {
+            _order = new Torder();
+        }
+        [DisplayName("訂單編號")]
+        public int Oid 
+        { 
+            get { return _order.Oid; }
+            set { _order.Oid = value; }
+        }
+        [DisplayName("訂購人")]
+        public string Oname
+        {
+            get { return _order.Oname; }
+            set { _order.Oname = value; }
+        }
+        [DisplayName("折價金額")]
+        public int? Odiscountedprice
+        {
+            get { return _order.Odiscountedprice; }
+            set { _order.Odiscountedprice = value; }
+        }
+        [DisplayName("合計")]
+        public int OtotalPrice
+        {
+            get { return _order.OtotalPrice; }
+            set { _order.OtotalPrice = value; }
+        }
+        [DisplayName("訂購時間")]
+        public DateTime Odate
+        {
+            get { return _order.Odate; }
+            set { _order.Odate = value; }
+        }
+        [DisplayName("商品編號")]
+        public int Mid
+        {
+            get { return _order.Mid; }
+            set { _order.Mid = value; }
+        }
+        [DisplayName("訂購人地址")]
+        public string Oaddress
+        {
+            get { return _order.Oaddress; }
+            set { _order.Oaddress = value; }
+        }
+        [DisplayName("訂購人電話")]
+        public string Ophone
+        {
+            get { return _order.Ophone; }
+            set { _order.Ophone = value; }
+        }
+        [DisplayName("訂單狀態")]
+        public string Ostatus
+        {
+            get { return _order.Ostatus; }
+            set { _order.Ostatus = value; }
+        }
+        [DisplayName("付款狀態")]
+        public bool Opayment
+        {
+            get { return _order.Opayment; }
+            set { _order.Opayment = value; }
+        }
+    }
+}
