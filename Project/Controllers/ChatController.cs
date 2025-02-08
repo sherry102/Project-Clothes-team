11f1aac156968accb8003c3f8c9a62e56e527f84
學舜 呂 <StyleOfMe1027@gmail.com>
2025-02-08 21:58:31 +0800

    新增前端聊天小視窗

--- Project/Controllers/ChatController.cs
+++ Project/Controllers/ChatController.cs
@@ -1,36 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Humanizer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Project.Models;
 
 namespace Project.Controllers
 {
     public class ChatController : Controller
     {
+        private readonly DbuniPayContext _dbuniPayContext;
         public IActionResult ChatRoom()
         {
             return View();
         }
 
-		public IActionResult test()
-		{
-			DbuniPayContext db = new DbuniPayContext();
-            //ViewBag.chatPeople = (from message in db.ChatMessages
-            //					 join member in db.Tmembers on message.MemberId equals member.Mid
-            //					 group new { message,member } by message.MemberId into grouped
-            //					 select new {
-            //						MId = grouped.Key,
-            //						MemberName = grouped.FirstOrDefault().member.Mname,
-            //						MemberPhoto =  grouped.FirstOrDefault().member.Mphoto,
-            //					 })
-            //					 .ToList();
+		//public IActionResult test()
+		//{
+		//	DbuniPayContext db = new DbuniPayContext();
+            
+  //          return View();
+  //      }
 
-
-            //var chatMessageViewModel = new ChatMessageViewModel
-            //{
-            //	chatMessages = db.ChatMessages.ToList()
-            //};
-
-            //return View(chatMessageViewModel);
-            return View();
+        // 取得會員資訊
+        [HttpGet]
+        public IActionResult GetMemberInfo()
+        {
+            if (!Int32.TryParse(HttpContext.Session.GetString(CDictionary.SK_LOGEDIN_USER), out int memberId))
+            {
+                return Json(new { success = false, message = "用戶未登入。" });
+            }
+            var member = _dbuniPayContext.Tmembers.FirstOrDefault(p => p.Mid == memberId);
+            if (member == null)
+            {
+                return Json(new { success = false, message = "會員資料不存在。" });
+            }
+            return Json(new
+            {
+                success = true,
+                data = new
+                {
+                    id = member.Mid,
+                    name = member.Mname,
+                    gender = member.Mgender,
+                    account = member.Maccount,
+                    email = member.Memail,
+                    address = member.Maddress,
+                    birthday = member.Mbirthday.ToString("yyyy-MM-dd"),
+                    phone = member.Mphone,
+                    points = member.Mpoints,
+                    permissions = member.Mpermissions,
+                    createdDate = member.McreatedDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                    photo = member.Mphoto,
+                    isHided = member.MisHided
+                }
+            });
         }
-	}
+    }
 }
