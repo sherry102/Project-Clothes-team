6ae6702d5f2164c44beb66a474e30e41c3dad53e
joey <joey4755@gmail.com>
2025-02-24 16:01:57 +0800

    修改

---
+++ Project/DTO/OpenAdviceDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace Project.DTO
+{
+    public class OpenAdviceDTO
+    {
+        public int Id { get; set; }
+
+        public int Mid { get; set; }
+
+        public int Oid { get; set; }
+
+        public string Question { get; set; } = null!;
+
+        public string Title { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public DateTime DateTime { get; set; }
+
+        public bool IsReply { get; set; }
+
+        public string email { get; set; }
+    }
+}
